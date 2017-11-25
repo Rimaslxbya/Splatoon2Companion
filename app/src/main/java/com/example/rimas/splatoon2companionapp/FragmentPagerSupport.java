4361630c183822252a7c3ad771c79bf739a5c9b6
Rimaslxbya <rimaslxbya@gmail.com>
2017-11-24 20:23:37 -0600

    Fixed: Clothes not being saved to database on close

--- app/src/main/java/com/example/rimas/splatoon2companionapp/FragmentPagerSupport.java
+++ app/src/main/java/com/example/rimas/splatoon2companionapp/FragmentPagerSupport.java
@@ -508,9 +508,11 @@ public class FragmentPagerSupport extends FragmentActivity {
         }
 
         @Override
-        public void onPause() {
-            super.onPause();
-            saveGearSelection();
+        public void setUserVisibleHint(boolean isVisibleToUser){
+            super.setUserVisibleHint(isVisibleToUser);
+
+            if(!isVisibleToUser && getContext() != null)
+                saveGearSelection();
         }
 
         /**
